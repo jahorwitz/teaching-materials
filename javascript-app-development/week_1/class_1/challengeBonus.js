c9fae11c14e561c9803520c747bd4efc3ec76f18
rafaeld101 <56375570+rafaeld101@users.noreply.github.com>
2019-12-10 20:54:32 -0500

    Update javascript-app-development/week_1/class_1/challengeBonus.js
    Co-Authored-By: Joshua Horwitz <jhorwitz@ebsco.com>

--- javascript-app-development/week_1/class_1/challengeBonus.js
+++ javascript-app-development/week_1/class_1/challengeBonus.js
@@ -4,6 +4,6 @@ if (num % 3 === 0) {
   console.log('fizz');
 } else if (num % 5 === 0) {
   console.log('buzz');
-} else if (num % 3 === 0 && num % 5 === 0) {
+} else if (num % 3 === 0 && num % 5 === 0 && num > 1) {
   console.log('fizzbuzz');
 }
