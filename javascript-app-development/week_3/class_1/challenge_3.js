f859441104e8871010fc30af8e98ac545d768f54
Joshua Horwitz <jhorwitz@ebsco.com>
2019-12-16 20:05:26 -0500

    added exercises for week 3, class 1

---
+++ javascript-app-development/week_3/class_1/challenge_3.js
@@ -0,0 +1,6 @@
+// Create an empty property named playInterval
+// A play() function that moves to the next photo ever 2000ms until the end.
+//     Tip - use playInterval = setInterval(fn,ms).
+// A pause() function that stops the slideshow
+//     Tip - use clearInterval(playInterval)
+// Automatically pause the slideshow if it gets to the end of the photolist while playing.
